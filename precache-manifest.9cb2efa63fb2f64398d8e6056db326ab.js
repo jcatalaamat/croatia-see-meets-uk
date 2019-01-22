660151a4ef8ca420a27203e236ff29d38aec9ae3
Jordi Catala <jordicatalaamat@gmail.com>
2019-01-22 01:12:02 +0100

    Update precache-manifest.9cb2efa63fb2f64398d8e6056db326ab.js

--- precache-manifest.9cb2efa63fb2f64398d8e6056db326ab.js
+++ precache-manifest.9cb2efa63fb2f64398d8e6056db326ab.js
@@ -1,66 +1,66 @@
 self.__precacheManifest = [
   {
     "revision": "c5b9f5d12ab9d47d6f123802a62892b7",
-    "url": "/croatia-see-meets-uk/static/media/Croatia_event_web_2-10.c5b9f5d1.png"
+    "url": "/static/media/Croatia_event_web_2-10.c5b9f5d1.png"
   },
   {
     "revision": "87f2e949107ca2b71570",
-    "url": "/croatia-see-meets-uk/static/css/main.5636391f.chunk.css"
+    "url": "/static/css/main.5636391f.chunk.css"
   },
   {
     "revision": "c141c4933336e00de052",
-    "url": "/croatia-see-meets-uk/static/js/1.c141c493.chunk.js"
+    "url": "/static/js/1.c141c493.chunk.js"
   },
   {
     "revision": "683d1821ec5dd19d7f6d",
-    "url": "/croatia-see-meets-uk/static/js/runtime~main.683d1821.js"
+    "url": "/static/js/runtime~main.683d1821.js"
   },
   {
     "revision": "540fa401dadee1beb8e685d14ffc80d8",
-    "url": "/croatia-see-meets-uk/static/media/citiy-bw_op.540fa401.jpg"
+    "url": "/static/media/citiy-bw_op.540fa401.jpg"
   },
   {
     "revision": "1b312cf1194011bb065873bfba298c1a",
-    "url": "/croatia-see-meets-uk/static/media/coworking_space_bw_op.1b312cf1.jpg"
+    "url": "/static/media/coworking_space_bw_op.1b312cf1.jpg"
   },
   {
     "revision": "3a9006517d08ba0412ed1b4df25ab2d0",
-    "url": "/croatia-see-meets-uk/static/media/coworking_bw_op.3a900651.jpg"
+    "url": "/static/media/coworking_bw_op.3a900651.jpg"
   },
   {
     "revision": "6fe98b028af44dc48f6a99874852ab17",
-    "url": "/croatia-see-meets-uk/static/media/gerkhin-bw_op.6fe98b02.jpg"
+    "url": "/static/media/gerkhin-bw_op.6fe98b02.jpg"
   },
   {
     "revision": "87f2e949107ca2b71570",
-    "url": "/croatia-see-meets-uk/static/js/main.87f2e949.chunk.js"
+    "url": "/static/js/main.87f2e949.chunk.js"
   },
   {
     "revision": "0d761771026fe81bf235138e173d729b",
-    "url": "/croatia-see-meets-uk/static/media/Croatia_event_web_2-11.0d761771.png"
+    "url": "/static/media/Croatia_event_web_2-11.0d761771.png"
   },
   {
     "revision": "d1d39692a24a44f0214202a10194bceb",
-    "url": "/croatia-see-meets-uk/static/media/Croatia_event_web_2-06.d1d39692.png"
+    "url": "/static/media/Croatia_event_web_2-06.d1d39692.png"
   },
   {
     "revision": "66cdc33bf8fad00d5ed4a69d5ed26cee",
-    "url": "/croatia-see-meets-uk/static/media/Croatia_event_web_2-07.66cdc33b.png"
+    "url": "/static/media/Croatia_event_web_2-07.66cdc33b.png"
   },
   {
     "revision": "906131ca2c663c63bfbecca8fe24c619",
-    "url": "/croatia-see-meets-uk/static/media/Croatia_event_web_2-09.906131ca.png"
+    "url": "/static/media/Croatia_event_web_2-09.906131ca.png"
   },
   {
     "revision": "b7a8b4dc8eeabe8c211b2fda8959fe54",
-    "url": "/croatia-see-meets-uk/static/media/Croatia_event_web_2-12.b7a8b4dc.png"
+    "url": "/static/media/Croatia_event_web_2-12.b7a8b4dc.png"
   },
   {
     "revision": "7c7ea8866b2c8ee2426fb678624b32ee",
-    "url": "/croatia-see-meets-uk/static/media/Croatia_event_web_2-08.7c7ea886.png"
+    "url": "/static/media/Croatia_event_web_2-08.7c7ea886.png"
   },
   {
     "revision": "e927e3e0d6c2199096c8ae56807910da",
-    "url": "/croatia-see-meets-uk/index.html"
+    "url": "/index.html"
   }
 ];

4998ca5ca6cf762e2b45ba4e61953f988393534c
Jordi Catala <jordicatalaamat@gmail.com>
2019-01-22 19:01:00 +0100

    board of directors

--- src/components/BoardOfDirectors.jsx
+++ src/components/BoardOfDirectors.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import RedHeader from './RedHeader';
+import Day from './Day';
 
 const BoardOfDirectors = () => (
   <div className="board-directors">
@@ -8,67 +9,100 @@ const BoardOfDirectors = () => (
       <div className="">
         <div className="history">
           <div className="history-one">
+            <Day text="Tony Suchy" />
             <p>
-            Established in 1998, the BCCC is a long-standing friend of
-British and Croatian businesses alike. With members from
-a wide range of sectors and industries we have hosted
-high-level Croatian officials on their first visits to the UK
-and introduced, via our extensive network, business
-contacts that have opened UK doors for many Croatian
-companies. We have also assisted British businesses into
-the Croatian market with its opportunities and
-ever-increasing potential as a gateway to the region.
-
+            Tony Suchy is a retired solicitor having been a partner in a
+City firm for more than 30 years. Tony has considerable
+interests and experience of Croatia and was instrumental
+in setting up the British Croatian Chamber of Commerce
+more than 20 years ago and has participated in its operations throughout that period.
             </p>
-            <p>
-            As British-Croatian business relations have evolved, so
-has our focus. Both British and Croatian businesses are
-continually seeking every possible opportunity for growth
-and cross-border expansion. Croatia will soon be entering
-the Eurozone; it is also the first westward EU port of call
-for all the Western Balkans countries lining up for EU
-membership. On the other hand Britain is an under-utilised
-market for Croatian business. If you are just starting up,
-the BCCC is the place to pitch your project. If you are
-growing, it’s a network that will multiply your efforts.
 
+            <Day text="Vlaho Kojakovic" />
+            <p>
+            Vlaho is Director of the Property and Tourism Team at the
+European Bank for Reconstruction and Development
+where he has been working for the past 7 years.
+              <br />
+              <br />
+He is a seasoned banker and a real estate executive with
+more than 20 years of experience in the sector.
+              <br />
+              <br />
+Before joining EBRD, Vlaho worked as Development Director with the Trigranit Development Corporation, the biggest real estate developer in Central and Southeast
+Europe. He started his career in investment banking with
+BNP Paribas in London.
+              <br />
+              <br />
+Vlaho ‎holds an MBA from Imperial College, University of
+London, and an executive education from the Kennedy
+School of Government, Harvard University.
+            </p>
+
+            <Day text="Damir Cimer" />
+            <p>
+            Damir is the Advisor to the Director on the Board of
+European Bank for Reconstruction and Development,
+representing Hungary, Czech Republic, Slovakia, Croatia
+and Georgia where he has been working for the past 3
+years. Before joining EBRD he was the advisor to the
+Croatian Vice-Prime Minister where he led a
+cross-ministerial team of experts working on wide reaching
+national reforms. For years he has been actively
+supporting and consulting public and private sector
+stakeholders, cooperating with OECD, in South East
+Europe and Caucuses on establishing institutional and
+legislative systems for Public Private Partnerships and
+Concessions.
             </p>
 
           </div>
           <div className="history-two">
+            <Day text="Stephen Ashurst" />
             <p>
-            Against this backdrop we seek to employ our
-well-established methods, contacts and knowledge to
-maximise and develop British-Croatian business
-opportunities.
-
+            Stephen is a contractor helping directors and executive
+VPs in the wealth, insurance, asset servicing, management consultancy and investment/fund management industries to build and run online retail investment platforms,
+wealth propositions and tax-wrapped products with more
+than 20 years of experience. His experience has been
+earned working in the UK and internationally with major
+management consultancies as an associate director and
+subject-matter expert. Stephen's record of accomplishment spans multiple clients including Nationwide, Lloyds
+Bank, EY, Fidelity, Barclays, BNP Paribas, Legal & General, the Financial Conduct Authority, start-ups & new ventures.
             </p>
+            <Day text="Lindsay Forbes" />
             <p>
-            A number of other organisations with similar missions are
-our fellow-travellers. Joining forces with our friends is more
-important than ever, for which reason we invite all those
-with Croatian and British interests at heart to reach out to
-us to collaborate and engage in joint events and activities.
-
+            Former Director at European Bank for Reconstruction and
+Development. Joined the Bank in 1994 and focused initially on industrial projects in Croatia. Subsequently managed
+the Corporate Equity activities of the Bank and lived for
+several years in Russia, representing the Bank on a
+number of Corporate and Bank Boards. Former Director of
+Pliva d.d.
             </p>
-            <div className="our-history__container--square">
-              <div className="box-red">
-                <h3 className="box-red__header">Join us</h3>
-                <div className="box-red__text-container">
-                  <span className="box-red__text">
-                  BCCC membership is open to all businesses and
-individuals, ranging from start-ups to high-profile,
-established companies who are actively interested in
-doing business between Croatia and the UK.
-                    <br />
-                    <br />
-For more information or to join BCCC, contact us here:
-                    <br />
-                    <br />
-                    <a style={{ textDecorationColor: '#e93732' }} href="mailto:info@britishcroatiancc.co.uk"><span className="p-contrast-red">info@britishcroatiancc.co.uk</span></a>
-                    <br />
-                    <br />
-                    <a style={{ textDecorationColor: '#e93732' }} href="mailto:embers@britishcroatiancc.co.uk"><span className="p-contrast-red">embers@britishcroatiancc.co.uk</span></a>
+
+            <Day text="Renaud Barbier Emery" />
+            <p>
+            Renaud is a solicitor and partner at Wikborg Rein in
+London. His practice focuses on the development of projects and the resolution of disputes in the shipping, shipbuilding and energy sectors. He has been assisting clients
+in Croatia for many years.
+            </p>
+
+
+            <div className="board-directors__container--square">
+              <div className="box-grey">
+                <div style={{ paddingLeft: '32px', paddingTop: '32px' }}>
+                  <Day text="Administration and Events" />
+                  <h3 className="box-grey__header">Dina Calakovic</h3>
+                </div>
+                <div className="box-grey__text-container">
+                  <span className="box-grey__text">
+                  Head of Events and Administration. Dina provides
+support for our Events and is responsible for
+liason with BCCC Members. Dina has a
+Bachelor's degree in Business & Economics and a
+Master's in Management from the University of
+Zagreb and a Certificate in Management
+Consultancy & Project Management at the London
+School of Business & Finance.
                   </span>
                 </div>
               </div>

75eebae8da26c08e0a5e239804eb0dbb30e7265a
Jordi Catala <jordicatalaamat@gmail.com>
2019-01-22 12:10:27 +0100

    react router and cname try

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import {
+  Route,
+  BrowserRouter,
+} from 'react-router-dom';
 import Page from './components/Page';
+import BcccPage from './components/BcccPage';
 import './App.scss';
 
 const App = () => (
-  <div className="App">
-    <Page />
-  </div>
+  <BrowserRouter>
+    <div className="App">
+      <Route exact path="/" component={BcccPage} />
+      <Route path="/tech_futures_london_event_2019" component={Page} />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/components/BcccPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BcccPage = () => (
+  <div>Hello World!</div>
+);
+
+export default BcccPage;

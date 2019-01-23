42a5ce1c238bd62d12ee5a6a6899caa51e32203b
Jordi Catala <jordicatalaamat@gmail.com>
2019-01-23 14:50:05 +0100

    Updates

--- precache-manifest.8aec7685bf4577cf8e84b6062a6c996b.js
+++ precache-manifest.8aec7685bf4577cf8e84b6062a6c996b.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/static/media/Croatia_event_web_2-10.c5b9f5d1.png"
   },
   {
-    "revision": "dbe647c9c741d46883a7",
-    "url": "/static/css/main.91b23ca3.chunk.css"
+    "revision": "ddab1c401f252efbadc8",
+    "url": "/static/css/main.4720b3d9.chunk.css"
   },
   {
     "revision": "9b0e93ec8b7293c95925",
@@ -16,12 +16,12 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "540fa401dadee1beb8e685d14ffc80d8",
-    "url": "/static/media/citiy-bw_op.540fa401.jpg"
+    "revision": "1d1c21bf064e68522606057dc4aa8f22",
+    "url": "/static/media/Hero-02_op.1d1c21bf.jpg"
   },
   {
-    "revision": "1b312cf1194011bb065873bfba298c1a",
-    "url": "/static/media/coworking_space_bw_op.1b312cf1.jpg"
+    "revision": "540fa401dadee1beb8e685d14ffc80d8",
+    "url": "/static/media/citiy-bw_op.540fa401.jpg"
   },
   {
     "revision": "3a9006517d08ba0412ed1b4df25ab2d0",
@@ -32,12 +32,16 @@ self.__precacheManifest = [
     "url": "/static/media/gerkhin-bw_op.6fe98b02.jpg"
   },
   {
-    "revision": "1d1c21bf064e68522606057dc4aa8f22",
-    "url": "/static/media/Hero-02_op.1d1c21bf.jpg"
+    "revision": "1b312cf1194011bb065873bfba298c1a",
+    "url": "/static/media/coworking_space_bw_op.1b312cf1.jpg"
   },
   {
-    "revision": "dbe647c9c741d46883a7",
-    "url": "/static/js/main.dbe647c9.chunk.js"
+    "revision": "ddab1c401f252efbadc8",
+    "url": "/static/js/main.ddab1c40.chunk.js"
+  },
+  {
+    "revision": "acb39a873881e1360f55b60df7837ac2",
+    "url": "/static/media/dubrovnik_bw_op.acb39a87.jpg"
   },
   {
     "revision": "d1d39692a24a44f0214202a10194bceb",
@@ -64,11 +68,7 @@ self.__precacheManifest = [
     "url": "/static/media/Croatia_event_web_2-12.b7a8b4dc.png"
   },
   {
-    "revision": "acb39a873881e1360f55b60df7837ac2",
-    "url": "/static/media/dubrovnik_bw_op.acb39a87.jpg"
-  },
-  {
-    "revision": "f9516308fd1393dbaef9851229c93447",
+    "revision": "e316b459773eb58cddf21a106f49178f",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ab76981181eb6e9548ee465abfb6554b.js"
+  "/precache-manifest.8aec7685bf4577cf8e84b6062a6c996b.js"
 );
 
 workbox.clientsClaim();

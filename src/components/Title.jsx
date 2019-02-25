864ba1006d89f7ba7cfe9b063b529b1d1c16ffbf
Jordi Catala <jordicatalaamat@gmail.com>
2019-02-24 22:02:34 -0800

    title change

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -68,7 +68,7 @@ technology.
           <p>
             <span className="p-contrast">
               <b>
-              Event is organised by British Croatian Chamber of Commerce, with support of Embassy of the Republic of Croatia in the UK.
+              Event is organised by British Croatian Chamber of Commerce, with support of Embassy of the Republic of Croatia in the UK and in partnership with British Serbian Chamber of Commerce.
               </b>
             </span>
           </p>

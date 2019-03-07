be07ac2ece68d48c21d67a60a4fa7421e378f285
Jordi Catala <jordicatalaamat@gmail.com>
2019-03-07 13:32:06 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3aa939bb05b616afa3a25ac391875eed.js"
+  "/precache-manifest.9a5cf28609406a67ab519e29f93feaa3.js"
 );
 
 workbox.clientsClaim();

654fcb6ae3051b6ecd16e4c3fdb63afe536b837d
Jordi Catala <jordicatalaamat@gmail.com>
2019-03-08 13:45:14 +0000

    new logo

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,6 +4,7 @@ import Popup from 'reactjs-popup';
 import HeaderMenu from './HeaderMenu';
 import AquaHeader from './AquaHeader';
 import BurgerIcon from './BurgerIcon';
+import Bullet from './Bullet';
 import Menu from './Menu';
 
 
@@ -41,13 +42,16 @@ const Title = (props) => {
         <div className="p-max">
           <p>
             <span className="p-contrast">
-            A unique 2-day event of professional CPD-accredited business education with world
-            class trainer and educator Gary Nuttall and associates on the basics and practical
-            implementation of distributed ledger technology, plus panel discussions on the
-            future of tech-family of innovations (FinTech, RegTech, HealthTech, GovTech),
-            pitching for Croatian and SEE start-ups and networking.
+            A unique 2-day event of
             </span>
+
+            <div className="p-contrast">
+              <Bullet text="Panel discussions on the future of tech-family of innovations (FinTech, RegTech, HealthTech, GovTech) with a focus on the SEE region" />
+              <Bullet text="Pitching for Croatian and SEE start-ups and networking, and" />
+              <Bullet text="professional CPD-accredited business education with world class trainer and educator Gary Nuttall and associates on the basics and practical implementation of distributed ledger technology" />
+            </div>
           </p>
+
           <p>
             <span className="p-contrast">
             The event is intended for Croatian, SEE and British business delegates (banking, law, healthcare, insurance,
@@ -58,24 +62,22 @@ digital futures and opportunities in SEE, European and global markets.
           <p>
             <span className="p-contrast">
               <b>
-              Taking place at TechSpace in the City of London's tech-fuelled 'Silicon Roundabout' area of Shoreditch, the event
-is offered as an organised package where the price includes participation in all the business and networking events,
-food and drinks throughout the 2 days and a CPD-accredited professional education certificate on distributed ledger
-technology.
+              Event is organised by British Croatian Chamber of Commerce;
               </b>
             </span>
-          </p>
-
-          <p>
-            <span className="p-contrast">
-              <b>
-              Event is organised by British Croatian Chamber of Commerce; with support of PwC Croatia, the auspices of Embassy of the Republic of Croatia in the UK, and in partnership with and British Serbian Chamber of Commerce.
-              </b>
-            </span>
+            <div className="p-contrast">
+              <Bullet text="With support of PwC Croatia," />
+              <Bullet text="With support of Department of International Trade," />
+              <Bullet text="Under the auspices of Embassy of the Republic of Croatia in the UK, and" />
+              <Bullet text="in partnership with and British Serbian Chamber of Commerce" />
+            </div>
           </p>
         </div>
+        <div className="entrepreneurs-logo" />
 
       </div>
+
+
       <div className="bottom-arrow" />
     </div>
   );
